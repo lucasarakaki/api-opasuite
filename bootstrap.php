a40b35c5531d2fc76ba32c1157ba241ddf27d535
Lucas Takeshi Arakaki <lucastakeshi@agilitytelecom.com.br>
2025-01-22 10:39:52 -0300

    fixed require for autoload, added require for helpers and loading of environment variables using Dotenv

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,14 @@
 <?php
 
-require __DIR__ . '/bootstrap.php';
+date_default_timezone_set('America/Fortaleza');
+
+// Require autoload do composer e funções
+require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/app/functions/helpers.php';
 
-date_default_timezone_set('America/Fortaleza');
+// Depêndencias
+use \Dotenv\Dotenv;
+
+// Carrega as variáveis de ambiente no .env
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
