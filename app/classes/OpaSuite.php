2614e607bdb81b1b02bd2d2577d06ccaea5197cb
Lucas Takeshi Arakaki <lucastakeshi@agilitytelecom.com.br>
2025-01-28 11:39:42 -0300

    Changed getById to private, added methods for getting user, department, and client names, and added addAdditionalInfo to enhance atendimento data

--- app/classes/OpaSuite.php
+++ app/classes/OpaSuite.php
@@ -82,7 +82,7 @@ class OpaSuite
      *
      * @return array Retorna a resposta da API em formato de array.
      */
-    public function getById(string $endpoint, string $id): array
+    private function getById(string $endpoint, string $id): array
     {
         // Url + endpoint da api
         $url = $this->baseUrl . '/' . ltrim($endpoint, '/') . '/' . ltrim($id, '/');
@@ -116,5 +116,75 @@ class OpaSuite
         curl_close($curl);
 
         return strlen($response) ? json_decode($response, true) : [];
+    }
+
+    /**
+     * Método reponsável por iterar os dados 'atendente', 'departamento' e 'cliente'
+     * @param array $atendimentos Array com os dados de atendimentos retornados da API
+     * @param array $usuarios Array com os dados de usuários retornados da API
+     *
+     * @return array Array com os novos dados
+     */
+    public function addAdditionalInfo(array $atendimentos, array $usuarios): array
+    {
+        foreach ($atendimentos['data'] as &$atendimento) {
+            $id_atendente = $atendimento['id_atendente'];
+            $id_cliente   = $atendimento['id_user'];
+            $id_dept      = $atendimento['setor'];
+
+            $atendente_nome    = $this->getUsername($usuarios, $id_atendente);
+            $cliente_nome      = $this->getClientName($id_cliente);
+            $departamento_nome = $this->getDeptName($id_dept);
+
+            $atendimento['atendente']    = $atendente_nome;
+            $atendimento['cliente']      = $cliente_nome;
+            $atendimento['departamento'] = $departamento_nome;
+        }
+
+        return $atendimentos;
+    }
+
+    /**
+     * Método responsável por localizar o nome do atendente
+     * @param array $usuarios Array com os dados dos usuários do Opa Suite
+     * @param string $id ID do atendente que deseja encontrar
+     *
+     * @return string Nome do atendente, ou mensagem de aviso.
+     */
+    private function getUsername(array $usuarios, string $id): string
+    {
+        foreach ($usuarios['data'] as $usuario) {
+            if ($usuario['_id'] === $id) {
+                return $usuario['nome'];
+            }
+        }
+
+        return 'Atendente não encontrado!';
+    }
+
+    /**
+     * Vai localizar o nome do departamento/setor
+     * @param string $id ID do departamento que deseja encontrar
+     *
+     * @return string Nome do departamento, ou mensagem de aviso.
+     */
+    private function getDeptName(string $id): string
+    {
+        $departamento_data = $this->getById('departamento', $id);
+
+        return $departamento_data['data']['nome'] ?? 'Departamento não encontrado!';
+    }
+
+    /**
+     * Vai localizar o nome do cliente
+     * @param string $id ID do cliente que deseja encontrar
+     *
+     * @return string Nome do cliente, ou mensagem de aviso.
+     */
+    private function getClientName(string $id): string
+    {
+        $cliente_data = $this->getById('cliente', $id);
+
+        return $cliente_data['data']['nome'] ?? 'Cliente não encontrado!';
     }
 }
