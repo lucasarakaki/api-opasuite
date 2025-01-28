98e7ad17a6c14bce660e5103cf8db740868d6d44
Lucas Takeshi Arakaki <lucastakeshi@agilitytelecom.com.br>
2025-01-28 11:35:01 -0300

    Remove get_username and get_dept_name functions, add extract_fields to handle API data extraction

--- app/functions/helpers.php
+++ app/functions/helpers.php
@@ -1,33 +1,22 @@
 <?php
 
 /**
- * Vai localizar o nome do atendente
- * @param array $usuarios Array com os dados dos usuários do Opa Suite
- * @param string $id ID do atendente que deseja encontrar
+ * Extraí os dados retornando pela API
+ * @param mixed $item
  *
- * @return string Nome do atendente, ou mensagem de aviso.
+ * @return array
  */
-function get_username(array $usuarios, string $id): string
+function extract_fields($item): array
 {
-    foreach ($usuarios['data'] as $usuario) {
-        if ($usuario['_id'] === $id) {
-            return $usuario['nome'];
-        }
-    }
-
-    return 'Atendente não encontrado!';
-}
-
-/**
- * Vai localizar o nome do departamento/setor
- * @param object $opasuite Instância da classe OpaSuite
- * @param string $id ID do departamento que deseja encontrar
- *
- * @return string Nome do deparramento, ou mensagem de aviso.
- */
-function get_dept_name(object $opasuite, string $id): string
-{
-    $departamento_data = $opasuite->getById('departamento', $id);
-
-    return $departamento_data['data']['nome'] ?? 'Departamento não encontrado!';
+    return [
+        'descricacao'  => $item['descricao'],
+        'protocolo'    => $item['protocolo'],
+        'tags'         => $item['tags'],
+        'avaliacoes'   => $item['avaliacoes'],
+        'observacoes'  => $item['observacoes'],
+        'motivos'      => $item['motivos'],
+        'atendente'    => $item['atendente'],
+        'cliente'      => $item['cliente'],
+        'departamento' => $item['departamento'],
+    ];
 }
