9400d03394cb56a031add0a082a83f2ea94ca2ba
Lucas Takeshi Arakaki <lucastakeshi@agilitytelecom.com.br>
2025-01-28 16:15:33 -0300

    feat: added function convert_date()

--- app/functions/helpers.php
+++ app/functions/helpers.php
@@ -18,5 +18,23 @@ function extract_fields($item): array
         'atendente'    => $item['atendente'],
         'cliente'      => $item['cliente'],
         'departamento' => $item['departamento'],
+        'date_start'   => $item['date_start'],
+        'date_end'     => $item['date_end'],
     ];
+}
+
+/**
+ * @param string $date Data para converter
+ *
+ * @return string Data convertida
+ */
+function convert_date(string $date): string
+{
+    $datetime = new DateTime($date);
+
+    $timezone = new DateTimeZone('America/Fortaleza');
+
+    $datetime->setTimezone($timezone);
+
+    return $datetime->format('d-m-Y H:i:s');
 }
