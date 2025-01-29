df7aacacece1678026cc99e05ce352c5cfc83733
Lucas Takeshi Arakaki <lucastakeshi@agilitytelecom.com.br>
2025-01-29 12:26:02 -0300

    added integration with the OpaSuite API to search for services and users, and generates reports in .xlsx format

--- public/index.php
+++ public/index.php
@@ -4,17 +4,28 @@ require __DIR__ . '/../bootstrap.php';
 
 use app\classes\OpaSuite;
 
-$filter = [];
+$filter = [
+    "dataInicialAbertura"   => "2025-01-29",
+    "dataFinalEncerramento" => "2025-01-29",
+];
 
-$options = [];
-
-$id = '';
+$options = [
+    /* "limit" => 10, // Limite comentado, pode ser removido se não for necessário */
+];
 
 try {
-    $opasuite = new OpaSuite();
-    /* $data = $opasuite->get('', $filter, $options);
+    $opasuite          = new OpaSuite();
+    $usuarios          = $opasuite->get('usuario', ['tipo' => 'user']);
+    $atendimentos      = $opasuite->get('atendimento', $filter, $options);
+    $atendimentos_data = $opasuite->addAdditionalInfo($atendimentos, $usuarios);
 
-    dd($data); */
+    // Extraindo os dados da API
+    $data = array_map('extract_fields', $atendimentos_data['data']);
+
+    // Guardando os dados em um arquivo .xlsx
+    include __DIR__ . '/../includes/xlsx.php';
+
 } catch (\Exception $e) {
+    // Debug
     dd($e->getMessage());
 }
